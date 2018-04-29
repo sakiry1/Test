59859fa2c76565df399958d788182b997f61087e
katte <kattnys@gmail.com>
2018-04-29 13:51:15 +0200

    3r cambio

--- app/src/main/java/com/miau/katherindenis/maps/map1.java
+++ app/src/main/java/com/miau/katherindenis/maps/map1.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class map1 extends AppCompatActivity   {
 
@@ -18,12 +19,15 @@ public class map1 extends AppCompatActivity   {
         setContentView(R.layout.activity_map1);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        TextView tx ;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+
             }
         });
     }
